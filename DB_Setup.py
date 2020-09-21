ef70887af994f95d42150698f4fc1dedd4fb3c28
prideven <70807326+prideven@users.noreply.github.com>
2020-09-21 19:44:42 +0530

    chnages with file api

--- DB_Setup.py
+++ DB_Setup.py
@@ -4,6 +4,10 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+import datetime
+from sqlalchemy import Column, Integer, DateTime
+from sqlalchemy import DateTime
+from sqlalchemy import func
 
 Base = declarative_base()
 
@@ -16,6 +20,19 @@ class StorageLogin(Base):
     password = Column(String(30), nullable=False)
     email = Column(String(50),nullable=False)
 
+class FileMetadata(Base):
+
+    __tablename__= "fileMetadata"
+    id = Column(Integer, primary_key=True)
+    file_name=Column(String(50),nullable=False)
+    loc=Column(String(550),nullable=False)
+    created = Column(DateTime, default=datetime.datetime.utcnow)
+    modified = Column(DateTime, default=datetime.datetime.utcnow)
+
+
+
+
+
 
     @property
     def serialize(self):
@@ -27,6 +44,17 @@ class StorageLogin(Base):
             'email': self.email,
         }
 
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id':self.id,
+            'file_name': self.file_name,
+            'loc': self.loc,
+            'modified': self.modified,
+            'created': self.created,
+
+        }
 
 engine = create_engine('sqlite:///Storage.db')
 
